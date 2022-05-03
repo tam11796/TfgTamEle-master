3c1beb00a67363d2eb569456216d5882b403fe70
msn_t <msn_tamara1996@hotmail.com>
2022-05-03 12:14:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tfg/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.tfg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//Prueba
+//qu3 5qe
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
